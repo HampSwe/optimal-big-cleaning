3912f0fccaa28fa2cefb54951e18587a3bf73984
Hampus Serneke <hampus.serneke@gmail.com>
2024-05-21 19:33:40 +0200

    Add Graph class

---
+++ big-cleaning/src/cleaning/Graph.java
@@ -0,0 +1,46 @@
+package cleaning;
+
+
+import java.util.*;
+
+public class Graph<T> {
+
+    private final Collection<Node<T>> nodes = new LinkedList<>();
+
+    public Node<T> addNode(T data) {
+        Node<T> node = new Node<>(data, new LinkedList<>());
+        nodes.add(node);
+        return node;
+    }
+
+    public void connectNodes (Node<T> u, Node<T> v) {
+        u.addNeighbour(v);
+    }
+
+    public Collection<Node<T>> getAllNodes() {
+        return nodes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Graph(\n");
+
+        for (Node<?> node : nodes) {
+            stringBuilder.append(node.toString() + "\n");
+        }
+
+        stringBuilder.append(")\n");
+
+        //stringBuilder.setLength(stringBuilder.length()-1);
+
+        return stringBuilder.toString();
+    }
+
+    private void resetGraph() {
+        for (Node<T> node : nodes) {
+            node.setVisited(false);
+        }
+    }
+}
+
